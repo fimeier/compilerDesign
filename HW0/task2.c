7371b20d507d60d57884649bb60654b8ab62872e
tobiahol <tobiahol@student.ethz.ch>
2017-03-02 16:36:33 +0100

    init commit

---
+++ HW0/task2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	/* read int from stdinp */
+	int in = 0;
+	printf("give input: ");
+	scanf("%d", &in);
+	
+	/* assembly part */
+	
+	printf("%d\n",in);
+	
+	return 0;
+	
+}
